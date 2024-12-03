963b1822d081d5a4d8cedd46966622ea3460cb0e
ac-14 <acaballerom@alumnos.nebrija.es>
2024-12-03 08:31:20 +0100

    second commit

--- main.ts
+++ main.ts
@@ -3,13 +3,11 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import {schema} from './schema.ts'
 import { resolvers } from "./resolvers.ts";
+import { flightModel } from "./types.ts";
 
 // Connection URL
 const MONGO_URL = Deno.env.get("MONGO_URL");
-if(!MONGO_URL){
-  console.log("URL not stablished");
-  Deno.exit(1);
-}
+
 const client = new MongoClient(MONGO_URL);
 
 // Database Name
@@ -18,7 +16,7 @@ const dbName = 'AD_2';
 await client.connect();
 console.log('Connected successfully to server');
 const db = client.db(dbName);
-const flightsCollection = db.collection('vuelos');
+const flightsCollection = db.collection<flightModel>('vuelos');
 
 const server = new ApolloServer({
   typeDefs: schema,
